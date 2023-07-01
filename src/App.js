c248ea915194ba29f387167598367a74f5cc1020
jchack1 <55161597+jchack1@users.noreply.github.com>
2023-06-30 23:40:07 -0600

    update styling;add background image, loading icon, fade animation; clean-up code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,121 @@
-import logo from "./logo.svg";
 import "./App.css";
 import {React, useState, useEffect} from "react";
 import styled from "styled-components";
 
-// still need to:
-// icons for weather and loading
-// organize code
-// general styling
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  flex-direction: column;
+  padding: 20px;
+
+  text-align: center;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  @media (max-width: 415px) {
+    h1 {
+      font-size: 1.6rem;
+    }
+  }
+`;
 
 const WeatherTempContainer = styled.div`
   display: flex;
   max-width: 700px;
   justify-content: space-between;
+  align-items: end;
+
+  @media (max-width: 415px) {
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 const TempContainer = styled.div`
   display: flex;
   margin: 10px;
+
+  @media (max-width: 415px) {
+    margin: 0;
+  }
+`;
+
+const Temp = styled.p`
+  font-size: 7.5rem;
+  margin: 20px 0;
+
+  @media (max-width: 415px) {
+    margin: 0 8px 0 0;
+    font-size: 5.5rem;
+  }
+`;
+
+const WeatherImg = styled.img`
+  width: 10rem;
+
+  @media (max-width: 415px) {
+    width: 4rem;
+  }
 `;
 
 const Degrees = styled.p`
   cursor: pointer;
   font-size: 2rem;
-  opacity: 90%;
-  margin-top: 92px;
+  opacity: 70%;
+  margin-top: 47px;
+
+  @media (max-width: 415px) {
+    margin-top: 15px;
+    font-size: 1.8rem;
+  }
 `;
 
 const WeatherContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin: 10px;
-`;
 
-const WeatherMainContainer = styled.div`
-  font-size: 1.2rem;
-  text-align: center;
-  margin-top: 0;
-
-  p {
-    margin-top: 0;
+  img {
+    filter: ${(props) =>
+      props.daylight
+        ? "drop-shadow(5px 5px 16px #717ab3)"
+        : "drop-shadow(5px 5px 16px #121314)"};
   }
 `;
+
+const DescriptionContainer = styled.div`
+  fontsize: 1.2rem;
+`;
+
+const LoadingIcon = () => {
+  return (
+    <div className="loading-icon">
+      <svg
+        aria-hidden="true"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={1.5}
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+};
 
 function App() {
   const [locationName, updateLocationName] = useState("");
@@ -48,17 +123,16 @@ function App() {
   const [weatherMain, updateWeatherMain] = useState("");
   const [temperature, updateTemperature] = useState("");
   const [weatherDescription, updateWeatherDescription] = useState("");
-  const [time, updateTime] = useState("");
   const [icon, updateIcon] = useState("");
   const [isCelsius, updateIsCelsius] = useState(true);
 
   const [apiError, updateApiError] = useState(false);
+  const [loading, updateLoading] = useState(false);
 
   const fetchLocationData = async (url) => {
     const response = await fetch(url);
 
     const jsonData = await response.json();
-    console.log(jsonData);
 
     if (!response.ok || jsonData.error) {
       updateApiError("Something went wrong");
@@ -76,17 +150,6 @@ function App() {
     return false;
   };
 
-  const formatTimestamp = (timestamp) => {
-    const dateObject = new Date(Number(timestamp * 1000));
-
-    const formatted = new Intl.DateTimeFormat("en-CA", {
-      hour: "2-digit",
-      minute: "2-digit",
-    }).format(dateObject);
-
-    return formatted;
-  };
-
   const convertCelsiusFahrenheit = (currentIsCelsius, temperature) => {
     if (currentIsCelsius) {
       updateTemperature(Math.round(temperature * 1.8 + 32));
@@ -101,6 +164,8 @@ function App() {
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async function (position) {
+      updateLoading(true);
+
       const url = `https://weather-proxy.freecodecamp.rocks/api/current?lon=${position.coords.longitude}&lat=${position.coords.latitude}`;
 
       const data = await fetchLocationData(url);
@@ -116,59 +181,67 @@ function App() {
       updateWeatherMain(data.weather[0].main);
       updateTemperature(Math.round(data.main.temp));
       updateWeatherDescription(data.weather[0].description);
-      updateTime(() => formatTimestamp(data.dt));
       updateIcon(data.weather[0].icon);
+
+      updateLoading(false);
     });
   }, []);
 
   // need loading icon
 
   if (apiError) {
-    return <div>{apiError}</div>;
+    return <PageContainer className={daylight ? "daylight" : "nighttime"} />;
   }
 
-  if (locationName.length === 0) {
-    return <div>no location to show</div>;
+  if (locationName.length === 0 && !loading) {
+    return (
+      <PageContainer className={daylight ? "daylight" : "nighttime"}>
+        <h1 className="fade-in">your local weather</h1>
+
+        <p className="fade-in">
+          allow location access to view your local weather{" "}
+        </p>
+      </PageContainer>
+    );
+  }
+
+  if (loading) {
+    return (
+      <PageContainer className={daylight ? "daylight" : "nighttime"}>
+        <LoadingIcon />
+      </PageContainer>
+    );
   }
 
   return (
-    <div className={daylight ? "daylight container" : "nighttime container"}>
-      <h1>your local weather</h1>
+    <PageContainer className={daylight ? "daylight" : "nighttime"}>
+      <h1 className="fade-in">{locationName}</h1>
 
-      <>
-        <div className="location-container">
-          <p>{locationName}</p>
-        </div>
+      <WeatherTempContainer className="fade-in">
+        <WeatherContainer>
+          <WeatherImg
+            src={icon}
+            alt={`${weatherMain} icon`}
+            daylight={daylight}
+          />
+        </WeatherContainer>
 
-        <WeatherTempContainer>
-          <WeatherContainer>
-            <div>
-              <img src={icon} alt={`${weatherMain} icon`} />
-            </div>
-            <WeatherMainContainer>
-              <p>{weatherMain}</p>
-            </WeatherMainContainer>
-          </WeatherContainer>
+        <TempContainer className="fade-in">
+          <Temp>{temperature}</Temp>
 
-          <TempContainer>
-            <p style={{fontSize: "5rem"}}>{temperature}</p>
+          <Degrees
+            onClick={() => convertCelsiusFahrenheit(isCelsius, temperature)}
+          >
+            &deg;
+            {isCelsius ? "C" : "F"}
+          </Degrees>
+        </TempContainer>
+      </WeatherTempContainer>
 
-            <Degrees
-              onClick={() => convertCelsiusFahrenheit(isCelsius, temperature)}
-            >
-              &deg;
-              {isCelsius ? "C" : "F"}
-            </Degrees>
-          </TempContainer>
-        </WeatherTempContainer>
-
-        <div className="time-container">
-          <p>{time}</p>
-        </div>
-      </>
-
-      {/* <p>{weatherDescription}</p> */}
-    </div>
+      <DescriptionContainer className="description-container fade-in">
+        <p>{weatherDescription}</p>
+      </DescriptionContainer>
+    </PageContainer>
   );
 }
 
